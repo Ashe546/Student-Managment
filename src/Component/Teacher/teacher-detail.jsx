4850f10fe74cc7db526f08171475a6bcfda30eb2
Ashe546 <asfyd42@gmail.com>
2022-11-25 03:00:49 -0300

    added teacher Detail

--- src/Component/Teacher/teacher-detail.jsx
+++ src/Component/Teacher/teacher-detail.jsx
@@ -4,7 +4,7 @@ export default function Teacherdetail() {
   return (
     <div class="w-full pt-10 px-4 sm:px-6 md:px-8 lg:pl-72">
       <h1>Teacher Detail \ <span className='text-amber-500'>Steven Johnson</span></h1>
-      <div className='bg-gray-200 p-4 mt-10'>
+      <div className='bg-gray-200 p-4 pb-10 mt-10'>
         <div className='flex w-full justify-between'>
           <h1>About Me</h1>
           <h1>...</h1>
@@ -13,21 +13,36 @@ export default function Teacherdetail() {
         <div class="grid grid-flow-col w-fit gap-16 mt-3">
           <div class="w-fit"><img src="/teacher.jpg" alt="" /></div>
           <div class="w-2/3">
-            <h1>Steven Johnson</h1>
+            <h1 className='mb-5 font-bold'>Steven Johnson</h1>
             <p>Aliquam erat volutpat. Curabiene natis
               massa sedde lacu stiquen sodale word moun
               taiery.Aliquam erat volutpaturabiene natis
               massa sedde sodale word moun taiery.
             </p>
-            <div className='grid grid-cols-2 gap-2'>
+            <div className='mt-4 grid grid-cols-2 gap-2'>
               <div>
-                <h1>name: Steven Johnson</h1>
+                <h1>Name: <span className=' ml-2 font-bold'>Steven Johnson</span> </h1>
               </div>
               <div>
-                <h1>name: Steven Johnson</h1>
+                <h1>Gender: <span className=' ml-2 font-bold'>Male</span> </h1>
               </div>
               <div>
-                <h1>name: Steven Johnson</h1>
+                <h1>Middle Name: <span className=' ml-2 font-bold'>Steve Johnson</span> </h1>
+              </div>
+              <div>
+                <h1>Mother Name: <span className=' ml-2 font-bold'>Naomi Rose</span> </h1>
+              </div>
+              <div>
+                <h1>Subject: <span className=' ml-2 font-bold'>English</span> </h1>
+              </div>
+              <div>
+                <h1>Email: <span className=' ml-2 font-bold'>1234@gmail.com</span> </h1>
+              </div>
+              <div>
+                <h1>Class: <span className=' ml-2 font-bold'>3B</span> </h1>
+              </div>
+              <div>
+                <h1>Address: <span className=' ml-2 font-bold'>Addis Abeba / Ethiopia</span> </h1>
               </div>
             </div>
           </div>
